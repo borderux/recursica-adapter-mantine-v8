569feb5316c5bfbcad878c67cf42aa847603d25c
Matt Massey <mattlmassey@gmail.com>
2026-08-26 21:07:49 -0700

    Updated postcss for library build (#527)
    Co-authored-by: Matt Massey <matt@borderux.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,7 @@ const config = createMainConfig({
   recursicaCSSPath: require.resolve(
     "@recursica/official-release/recursica_variables_scoped.css",
   ),
+  postCSSStrictMode: process.env.NODE_ENV === "production",
 });
 
 config.typescript = {
